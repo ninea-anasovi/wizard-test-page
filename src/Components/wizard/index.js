a676bd8535f7cbe98553e1279b1dbb712e73e2a1
Ninea Anasovi <ninea.anasovi@gmail.com>
2022-05-28 00:39:14 +0400

    edited input field

--- src/Components/wizard/index.js
+++ src/Components/wizard/index.js
@@ -8,14 +8,24 @@ import { WizardWrapper } from './styled'
 function Wizard() {
   return (
     <WizardWrapper>
-
       
       <Tooltip text='This Field Can Not Be Empty' position={'bottom'}>
         <TooltipTarget>
-          <Input text={'Name'} type={'Text'} /> 
-          <Checkbox text={'Agree'} type={'checkbox'} /> 
+          <Input text={'Name'} type={'Text'} placeHolder={'Your Name'}/> 
         </TooltipTarget>
       </Tooltip>
+      <Tooltip text='This Field Can Not Be Empty' position={'bottom'}>
+        <TooltipTarget>
+          <Input text={'Surname'} type={'Text'} placeHolder={'Your Surname'}/>  
+        </TooltipTarget>
+      </Tooltip>
+      <Tooltip text='This Field Can Not Be Empty' position={'bottom'}>
+        <TooltipTarget>
+          <Checkbox text={'Agree'} type={'checkbox'} />
+        </TooltipTarget>
+      </Tooltip>
+
+      
 
     </WizardWrapper>
 
--- src/Components/wizard/styled.js
+++ src/Components/wizard/styled.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
 export const WizardWrapper = styled.div`
-  position: relative;
   display: block;
 `;
